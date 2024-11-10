c164c0696ae501a5baf1f7dafed4c955f4dabbef
shafi209 <shafi.devops209@gmail.com>
2024-11-10 23:49:49 +0000

    added in notes

---
+++ Day-08/01-Notes/test.py
@@ -0,0 +1,16 @@
+#list 
+
+my_lists = ["test1", "test2", "test3"]
+
+#my_list_slice = my_lists[0:2]
+
+my_lists.append("test4")
+
+#if the list is in curly braces it will be tupple
+#ex my_lists = ("test1", "test2", "test3")
+
+
+my_lists.remove("test2")
+
+print(len(my_lists))
+print(my_lists)
