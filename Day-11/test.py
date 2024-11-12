c05e824bc21e67156923d4e86bc12a8d1f9cd47f
shafi209 <shafi.devops209@gmail.com>
2024-11-12 00:06:22 +0000

    updated the day 11

---
+++ Day-11/test.py
@@ -0,0 +1,20 @@
+students_info = [
+    {
+        "age": "01",
+        "class": "10",
+        "section": "A"
+    },
+    {
+        "age": "8",
+        "class": "30",
+        "section": "B"
+    },
+    {
+        "age": "5",
+        "class": "20",
+        "section": "C"
+    }
+]
+
+# Accessing the "class" key of the first dictionary
+print(students_info[0]["class"])
